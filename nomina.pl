540e0c0877a4d446b8fc56a6114699a076911d2d
anferente20 <34009575+anferente20@users.noreply.github.com>
2018-05-03 13:38:19 -0500

    funciona

--- nomina.pl
+++ nomina.pl
@@ -1,13 +1,26 @@
 persona( nombre('Pedro'), categoria(3), contrato( 'TC' ) ).
-persona( nombre('Pedra'), categoria(2), contrato( 'H' ) ).
-persona( nombre('Sebastian :v '), categoria(10), contrato( 'MT') ).
+persona( nombre('Pedra'), categoria(2), contrato( 'TC' ) ).
+persona( nombre('Sebastian :v '), categoria(10), contrato( 'TC') ).
 persona( nombre('David'), categoria(5), contrato( 'H' ) ).
 
-minimo(781771).
-horas(5000).
+% Pagos
+minimo(781242).
+hora(5000).
+
+% Descuentos
+salud(0.085).
+pension(0.12).
 
 % Salario por categoria
-salario(X,Y,Z,S):-persona( nombre(X), categoria(Y), contrato(Z) ), Z == 'TC', minimo(M), S is (Y* M ).
-salario(X,Y,Z,S):-persona( nombre(X), categoria(Y), contrato(Z) ), Z == 'MT', minimo(M), S is (Y* M/2 ).
-salario(X,Y,Z,H,S):-persona( nombre(X), categoria(Y), contrato(Z) ), Z == 'H', horas(M), S is (Y* M* H ).
+salario(X,Y,Z,S):-persona( nombre(X), categoria(Y), contrato(Z) ), Z == 'TC', minimo(M),salud(C), pension(P), S is (Y* M * (1- C) * (1- P) ).
+salario(X,Y,Z,S):-persona( nombre(X), categoria(Y), contrato(Z) ), Z == 'MT', minimo(M), S is ((Y* M/2)*(1- C) * (1- P) ).
+salario(X,Y,Z,H,S):-persona( nombre(X), categoria(Y), contrato(Z) ), Z == 'H', hora(M), S is (Y* M* H ).
 
+
+% descuento  por categoria
+descuento(X,Y,Z,S):-persona( nombre(X), categoria(Y), contrato(Z) ), Z == 'TC', minimo(M),salud(C), pension(P), S is (Y* M *  C *  P ).
+descuento(X,Y,Z,S):-persona( nombre(X), categoria(Y), contrato(Z) ), Z == 'MT', minimo(M), S is ((Y* M/2)* C * P ).
+
+
+nomina([],R):-R=0. 
+nomina([ X | Y ],R) :- sumar(Y, R1), R is R1+X.
